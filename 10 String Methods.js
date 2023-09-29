1980af1973ab9884cd8911b4ff6746ce1081aad5
chandan <chandankuma8001360@gmail.com>
2023-09-29 23:35:53 +0530

    800 level problem

---
+++ 10 String Methods.js
@@ -0,0 +1,6 @@
+let name = "chandan";
+console.log(name.length)
+console.log(name.toUpperCase())
+console.log(name.toLowerCase())
+console.log(name.slice(2,4))
+console.log(name.slice(2))
---
+++ 9 Strings.js
@@ -0,0 +1,11 @@
+let friend = "chandan"
+console.log(friend);
+
+let boy1="jitender"
+let boy2="Shubham"
+
+let sentence=`${boy1} is friend of ${boy2}`
+console.log(sentence);
+
+let fruit="bana\'na"
+console.log(fruit)
